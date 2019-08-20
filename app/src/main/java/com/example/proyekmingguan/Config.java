24a9ed49bb9d4e5a3f3531da33311c95c56b20e3
HeroVostox <syubbanfakhriya@gmai.com>
2019-08-20 10:41:51 +0700

    Full change

--- app/src/main/java/com/example/proyekmingguan/Config.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyekmingguan;
-
-class Config {
-    private static final String ROOT_URL = "http://192.168.43.213/MyApi/iniApi.php?apicall=";
-    static final String UPLOAD_URL = ROOT_URL + "uploadpic";
-    public static final String GET_PICS_URL = ROOT_URL + "getpics";
-}
